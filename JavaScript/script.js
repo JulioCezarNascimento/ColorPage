fe257e35ae4e66af1c0d1eacdb308db00f204d68
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2023-09-19 14:26:21 -0300

    Interação entre o index e JS estabelecida.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,9 @@
 const container = document.querySelector('.container');
 const color = document.querySelector('input');
+
+const defaultColor = '#000';
+container.style.backgroundColor = '${defaultColor}';
+
+function changeColor() {
+    container.style.backgroundColor = '${color.value}';
+}
