d68ebba5e1066ef694794b1dd9ab4e9e5866545d
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2023-09-21 16:22:23 -0300

    Atualização do container e function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('.container');
-const color = document.querySelector('input');
+const color = document.querySelector('#color'); // Corrigi o seletor para o ID correto
 
 const defaultColor = '#000';
-container.style.backgroundColor = '${defaultColor}';
+container.style.backgroundColor = `${defaultColor}`;
 
 function changeColor() {
-    container.style.backgroundColor = '${color.value}';
+    container.style.backgroundColor = `${color.value}`;
 }
